50483f949040eaf09be53a0b90dda0d4df2a363c
bilal-sagir <sagir.bilal@icloud.com>
2021-11-15 17:02:57 +0300

    zero

--- debugging/ViewController.swift
+++ debugging/ViewController.swift
@@ -1,17 +1,12 @@
-//
-//  ViewController.swift
-//  debugging
-//
-//  Created by Bilal on 15.11.2021.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
